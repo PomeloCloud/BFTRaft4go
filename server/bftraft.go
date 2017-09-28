cf049c6c011f2ecd15d946e6753242534f2dc96c
Jack Shi <shisoftgenius@gmail.com>
2017-09-29 06:18:18 +0800

    server startup

--- server/bftraft.go
+++ server/bftraft.go
@@ -3,6 +3,9 @@ package server
 import (
 	context "golang.org/x/net/context"
 	pb "github.com/PomeloCloud/BFTRaft4go/proto"
+	"flag"
+	"net"
+	"google.golang.org/grpc"
 )
 
 type BFTRaftServer struct {
@@ -19,4 +22,16 @@ func (s *BFTRaftServer) RequestVote(context.Context, *pb.RequestVoteRequest) (*p
 
 func (s *BFTRaftServer) AppendEntries(context.Context, *pb.AppendEntriesRequest) (*pb.AppendEntriesResponse, error)  {
 	return nil, nil
+}
+
+func start(address string) error {
+	flag.Parse()
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		return err
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterBFTRaftServer(grpcServer, &BFTRaftServer{})
+	grpcServer.Serve(lis)
+	return nil
 }
