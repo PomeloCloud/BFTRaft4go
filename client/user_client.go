a0a208d258709fda738274ebc2e70a76b07c319b
Jack Shi <shisoftgenius@gmail.com>
2017-10-06 03:43:11 +0800

    fmt

--- client/user_client.go
+++ client/user_client.go
@@ -1,14 +1,13 @@
 package client
 
 import (
+	"crypto/rsa"
 	pb "github.com/PomeloCloud/BFTRaft4go/proto/client"
 	serverPb "github.com/PomeloCloud/BFTRaft4go/proto/server"
-	"crypto/rsa"
 )
 
 type BFTRaftClient struct {
-	rpc *serverPb.BFTRaftClient
-	feedback *pb.BFTRaftClientServer
+	rpc        *serverPb.BFTRaftClient
+	feedback   *pb.BFTRaftClientServer
 	privateKey *rsa.PrivateKey
 }
-
