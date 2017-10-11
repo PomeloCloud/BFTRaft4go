af38f37831bb4cad99b832dd32890dc4fc044cc3
Jack Shi <shisoftgenius@gmail.com>
2017-10-11 16:38:01 +0800

    get group hosts on client

--- client/feedback_server.go
+++ client/feedback_server.go
@@ -1,5 +1,18 @@
 package client
 
+import (
+	"context"
+	pb "github.com/PomeloCloud/BFTRaft4go/proto/client"
+)
+
 type FeedbackServer struct {
 	clientIns *BFTRaftClient
 }
+
+func (fs *FeedbackServer) ResponseCommand(ctx context.Context, cmd *pb.CommandResult) (*pb.Nothing, error) {
+	// TODO: Verify signature
+	// signData := server.CommandSignData(cmd.Group, cmd.NodeId, cmd.RequestId, cmd.Result)
+	fs.clientIns.CmdResChan[cmd.Group][cmd.RequestId] <- cmd.Result
+	return &pb.Nothing{}, nil
+}
+
--- client/user_client.go
+++ client/user_client.go
@@ -1,18 +1,21 @@
 package client
 
 import (
-	"context"
 	"crypto/rsa"
 	spb "github.com/PomeloCloud/BFTRaft4go/proto/server"
 	"github.com/PomeloCloud/BFTRaft4go/server"
 	"github.com/PomeloCloud/BFTRaft4go/utils"
+	"github.com/patrickmn/go-cache"
 	"sync"
+	"context"
 )
 
 type BFTRaftClient struct {
 	Id         uint64
 	PrivateKey *rsa.PrivateKey
-	AlphaNodes []*spb.Node
+	AlphaNodes []*spb.BFTRaftClient
+	Groups     *cache.Cache
+	CmdResChan map[uint64]map[uint64]chan []byte
 	Lock       sync.RWMutex
 }
 
@@ -32,7 +35,27 @@ func NewClient(bootstraps []string, opts ClientOptions) (*BFTRaftClient, error) 
 		Id:         server.HashPublicKey(publicKey),
 		PrivateKey: privateKey,
 		Lock:       sync.RWMutex{},
+		AlphaNodes: []*spb.BFTRaftClient{},
 	}
-	bftclient.AlphaNodes = utils.AlphaNodes(bootstraps)
-	return nil, nil
+	bftclient.RefreshAlphaNodes(bootstraps)
+	return bftclient, nil
+}
+
+func (brc *BFTRaftClient) RefreshAlphaNodes(bootstraps []string) {
+	nodes := utils.AlphaNodes(bootstraps)
+	for _, node := range nodes {
+		if c, err := utils.GetClusterRPC(node.ServerAddr); err == nil {
+			brc.AlphaNodes = append(brc.AlphaNodes, &c)
+		}
+	}
+}
+
+func (brc *BFTRaftClient) GetGroupHosts(groupId uint64) *[]*spb.Host {
+	return utils.MajorityResponse(brc.AlphaNodes, func(client spb.BFTRaftClient) (interface{}, []byte) {
+		if res, err := client.GroupHosts(context.Background(), &spb.GroupId{GroupId: groupId}); err == nil {
+			return &res.Nodes, utils.NodesSignData(res.Nodes)
+		} else {
+			return nil, []byte{}
+		}
+	}).(*[]*spb.Host)
 }
