7695d972bfdb76f822c3f901c2f0c9a37e51fd00
Jack Shi <shisoftgenius@gmail.com>
2017-10-13 22:16:55 +0800

    signature test

---
+++ utils/utils_test.go
@@ -0,0 +1,42 @@
+package utils
+
+import "testing"
+
+func TestGenerateKey(t *testing.T) {
+	pri, pub, err := GenerateKey()
+	if err != nil {
+		panic(err)
+	}
+	priK, err := ParsePrivateKey(pri)
+	if err != nil {
+		panic(err)
+	}
+	pubK, err := ParsePublicKey(pub)
+	if err != nil {
+		panic(err)
+	}
+	pubFrPriv := PublicKeyFromPrivate(priK)
+	if pubK.E != pubFrPriv.E {
+		panic("key gen pub key not match")
+	}
+}
+
+func TestVerifySign(t *testing.T) {
+	pri, pub, err := GenerateKey()
+	if err != nil {
+		panic(err)
+	}
+	priK, err := ParsePrivateKey(pri)
+	if err != nil {
+		panic(err)
+	}
+	pubK, err := ParsePublicKey(pub)
+	if err != nil {
+		panic(err)
+	}
+	data := []byte("test signature")
+	sig := Sign(priK, data)
+	if err := VerifySign(pubK, sig, data); err != nil {
+		panic(err)
+	}
+}
